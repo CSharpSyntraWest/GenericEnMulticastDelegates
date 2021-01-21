9c2d5997ee3b7287915a05a851ceab10d7de938b
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-21 12:38:00 +0100

    Demo Multicast delegate

---
+++ MultiCastDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiCastDelegate
+{
+    public delegate int MyMultiCastDelegate<T>(T param);
+    class Program
+    {
+        static int PrintString(string tekst)
+        {
+            Console.WriteLine($"Printstring met param='{tekst}'");
+            return 1;
+        }
+
+        static int PrintStringLengte(string text)
+        {
+            Console.WriteLine($"PrinStringLengte met param='{text}' : lengte {text.Length}");
+            return 2;
+        }
+        static void Main(string[] args)
+        {
+            MyMultiCastDelegate<string> del_multicast = PrintString;
+            del_multicast += PrintStringLengte;
+
+            int result = del_multicast("een string");
+            Console.WriteLine("Terugkeerwaarde via multicast delegate: " + result);
+            Console.ReadKey();
+        }
+    }
+}
